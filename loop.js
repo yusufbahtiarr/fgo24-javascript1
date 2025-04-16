a9aedb9aebf8105e5d764c1a1f664d33a17cc25a
yusufbahtiarr <96324893+yusufbahtiarr@users.noreply.github.com>
2025-04-16 13:39:24 +0700

    Add files via upload

--- loop.js
+++ loop.js
@@ -1,7 +1,21 @@
+// For Loop
 for(let i=1; i <= 5; i++){
     if (i==4){
         console.log("fazztrack");   
     } else {
         console.log(i);   
+    }
 }
+
+// While Loop
+let x = 0
+while(x < 5){
+    let y = 5
+    let result = ""
+    while(y > x){
+        result += "*"
+        y--
+    }
+    console.log(result);
+    x++
 }
