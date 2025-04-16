d5019856702f5c4c5e7bf3888061fc21ed33cebb
yusufbahtiarr <96324893+yusufbahtiarr@users.noreply.github.com>
2025-04-16 10:46:08 +0700

    Add files via upload

---
+++ access-array.js
@@ -0,0 +1,20 @@
+//fgo24-javasript1
+const data = [
+    [ ,[ , , , {
+        string: [ ,{
+            value : "hello"
+        }]}]
+    ]
+
+]
+   
+console.log(data[0][1][3].string[1].value); // hello
+
+
+const test = [ {
+    string: [ ,{
+        value : "test"
+    }]}]
+console.log(test[0].string[1].value);
+
+    
